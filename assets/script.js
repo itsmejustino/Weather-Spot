7375770ee15ccaeb1b364b371a9f25b3f5a21a26
jdog <thuz3rfr13ndly@gmail.com>
2022-05-20 15:29:56 -0600

    framework and fetch ext forecast

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,31 @@
-// fetch API
-fetch('https://api.openweathermap.org/data/3.0/onecall?lat=39.51833126&lon=-104.759496962&appid=47659c28e771d77dde5b14c321e29f33')
-  .then(response => response.json())
-  //promise
-  .then(data => console.log(data));
 
 
-//-----HERO WEATHER -----  
+//-----HERO WEATHER -----
 //display City and current Date location with icon
 // get weather data temp, wind, humidity, and UV index and their values
 
-//-----eXTENDED FORECAST -----  
+// _____fetch API_____
+
+let currentFore = fetch(
+  "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=39.5183&lon=-104.7595&dt=1653080053&appid=861580140589909a9bfb6990ee8d23ba"
+)
+  .then((response) => response.json())
+
+  .then((data) => console.log(data));
+
+//-----eXTENDED FORECAST -----
 //get 5 day forecast weather. Temp, Wind speed in MPH, Humidity %
+//carosel for extended forcast??
+
+
+// _____fetch API_____
+
+let extendedFore = fetch(
+  "http://api.openweathermap.org/data/2.5/forecast?q=Parker,Denver,CO,USA&appid=861580140589909a9bfb6990ee8d23ba"
+)
+  .then((response) => response.json())
+
+  .then((data) => console.log(data));
 
 //-----SEARCH BAR-----
 //take user input and match city or zip code search to return weather data in HERO & eXTENDED FORECAST
