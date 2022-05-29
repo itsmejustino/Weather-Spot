3d465ea86493bf6552d1ba27e6c12e57940a35ab
jdog <thuz3rfr13ndly@gmail.com>
2022-05-29 00:51:30 -0600

    search query by city

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,46 @@
-//-----SEARCH BAR-----
-//take user input and match city or zip code search to return weather data in HERO & eXTENDED FORECAST
+
+//----!IMPORTANT KEY AND FETCH URL!----
+let apiKey = "47659c28e771d77dde5b14c321e29f33";
+let fetchURL = "https://api.openweathermap.org/data/2.5/"
+
+//button, input area, and data containers
 let searchBtn = document.getElementById("search-btn");
 let userSearch = document.getElementById("typed-search");
 let heroData = document.getElementById("hero-data");
-let weatherCard = document.getElementById("weath-car1");
+let weatherCard = document.getElementById("weather-card");
 
+//take user input and match city to query parameter
+//-----SEARCH BAR-----
 searchBtn.addEventListener("click", () => {
-  getCurrentWeather();
-  // getZipWeatherForecast();
-  displayCurrentWeather();
+  let cityValue = userSearch.value;
+  localStorage.setItem("city", cityValue);
+
+  showSearchedWeather() ;
 });
-function getCurrentWeather() {
-  //   // _____fetch API_____
 
+function showSearchedWeather() {
+// _____fetch API_____
   fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-      userSearch.value +
-      ",CO,us&appid=47659c28e771d77dde5b14c321e29f33"
+    `${fetchURL}weather?q=${userSearch.value}&appid=${apiKey}`
   )
     .then((response) => response.json())
 
-    .then((data) => console.log(data));
-  fetch(
-    "https://api.openweathermap.org/data/2.5/weather?zip=" +
-      userSearch.value +
-      ",us&appid=47659c28e771d77dde5b14c321e29f33"
-  )
-    .then((response) => response.json())
-
-    .then((data) => console.log(data));
+    .then((data) => {
+      console.log(data.coord.lat, data.coord.lon);
+      getWeatherData(data.coord.lat, data.coord.lon)
+    } );
+ 
 }
 
-getWeatherData();
-function getWeatherData() {
-  navigator.geolocation.getCurrentPosition((success) => {
-    console.log("success");
+function getWeatherData(latitude,longitude) {
 
-    let { latitude, longitude } = success.coords;
+  // navigator.geolocation.getCurrentPosition((success) => {
+  //   console.log(success);
+
+  //   let { latitude, longitude } = success.coords;
 
     fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=47659c28e771d77dde5b14c321e29f33`
+      `${fetchURL}onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${apiKey}`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -47,109 +48,60 @@ function getWeatherData() {
         showWeatherData(data);
         showExtendedWeatherData(data);
       });
-  });
-}
+    }
 
+//-----HERO WEATHER -----
+//display City and current Date location with icon
+// get weather data temp, wind, humidity, and UV index and their values. display for current searched by user.
+// ICON FOR CURRENT <img src = "https://openweathermap.org/img/wn/"icon@2x.png" alt = "Weather-icon" class = "weather-icon">
 function showWeatherData(data) {
-  let { temp, wind_speed, humidity, uvi} = data.current;
+  let { temp, feels_like, wind_speed, humidity, uvi, weather } = data.current;
+  // let icon = data.weather[0].icon;
   heroData.innerHTML = `<div class="city-data" id="hero-data">
-
      <p> Temperature: ${temp} °F <br>
+     Temperature Feels Like: ${feels_like}°F <br>
      Pressure: ${wind_speed}<br>
       Humidity: ${humidity}%<br>
+      UV Index: ${uvi}
       </p>      
 </div>`;
-
 }
 
+//-----EXTENDED FORECAST -----
+//get 5 day forecast weather. Temp, Wind speed in MPH, Humidity %
+ //<img src = "http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt = "Weather-icon" class = "weather-icon">
+let extendedForecast = "";
 function showExtendedWeatherData(data) {
-  let { temp, wind_speed, humidity} = data.daily;
-  let extendedForecast = "";
-
-  for (let i = 0; i < 6; i++) {
-    extendedForecast += data.daily[i];
+  for (let i = 0; i < 5; i++) {
+    let { temp, wind_speed, humidity, feels_like, day } = data.daily[i];
     console.log(extendedForecast);
-
     if (i == 0) {
-      extendedForecast += `
-    <div class="card-style1" id="weath-car1">
+      weatherCard.innerHTML = `
+    <div class="card-style" id="weather-card">
+   
       <p>
-      Temperature: ${temp}°F <br>
+      Temperature: ${temp.day}°F <br>
+      Temperature Feels Like: ${feels_like.day}°F <br>
       Pressure: ${wind_speed}<br>
        Humidity: ${humidity}%<br>
       </p>
     </div>
   `;
     } else {
-      extendedForecast += `
-  <div class="card-style1" id="weath-car1">
+      weatherCard.innerHTML += `
+  <div class="card-style" id="weather-card">
     <p>
-    Temperature: ${temp}°F <br>
+    Temperature: ${temp.day}°F <br>
+    Temperature Feels Like: ${feels_like.day}°F <br>
     Pressure: ${wind_speed}<br>
      Humidity: ${humidity}%<br>
 
     </p>
   </div>
 `;
-      weatherCard.innerHTML = extendedForecast;
     }
   }
 }
-// function getZipWeatherForecast() {
-//   // _____fetch API_____
-//   fetch(
-//     "https://api.openweathermap.org/data/2.5/forecast?zip=" +
-//       userSearch.value +
-//       ",us&appid=47659c28e771d77dde5b14c321e29f33"
-//   )
-//     .then((response) => response.json())
-
-//     .then((data) => console.log(data));
-//   fetch(
-//     "https://api.openweathermap.org/data/2.5/forecast?q=" +
-//       userSearch.value +
-//       ",CO,us&appid=47659c28e771d77dde5b14c321e29f33"
-//   )
-//     .then((response) => response.json())
-
-//     .then((data) => console.log(data));
-// }
-
-//-----HERO WEATHER -----
-//display City and current Date location with icon
-// get weather data temp,
-//wind, humidity, and UV index and their values. display for current searched by user.
-
-// function displayCurrentWeather() {
-//   fetch(
-//     "https://api.openweathermap.org/data/2.5/weather?q=" +
-//       userSearch.value +
-//       ",CO,us&appid=47659c28e771d77dde5b14c321e29f33"
-//   )
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       appendData(data);
-//     })
-//     .catch(function (err) {
-//       console.log("error: " + err);
-//     });
-
-//   function appendData(data) {
-//     let mainContainer = document.getElementById("hero-data");
-//     for (let i = 0; i < data.length; i++) {
-//     var heroDiv = document.createElement("div");
-//     heroDiv.innerHTML = "Weather: " + data[i].weather{main};
-//     mainContainer.appendChild(heroDiv);
-//     console.log(mainContainer);
-//   }
-// }
-
-// };
-
-//-----EXTENDED FORECAST -----
-//get 5 day forecast weather. Temp, Wind speed in MPH, Humidity %
 
 //-----QUICK SEARCH -----
 //Quick search of 5 cities that will fetch weather data to display HERO & eXTENDED FORECAST
